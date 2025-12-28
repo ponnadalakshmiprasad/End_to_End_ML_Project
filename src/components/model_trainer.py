bd68d72638b1c715425aa77ba456f1d4c98f2b87
Ponnada Lakshmi Prasad <ponnadaapparao6@gmail.com>
2025-12-29 00:44:06 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -6,7 +6,7 @@ from src.pipelines.exception import CustomException
 from src.pipelines.logger import logging
 from src.pipelines.utils import save_object, evaluate_models
 from dataclasses import dataclass
-
+from sklearn.model_selection import RandomizedSearchCV,GridSearchCV
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
@@ -44,8 +44,58 @@ class ModelTrainer:
                 "KNeighborsRegressor":KNeighborsRegressor()
             }
 
+            params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForestRegressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "Ridge": {
+                    "alpha": [0.1, 1.0, 10.0]
+                },
+                "Lasso": {
+                    "alpha": [0.001, 0.01, 0.1, 1.0]
+                },
+                "SVR": {
+                    "C": [0.1, 1, 10],
+                    "kernel": ["rbf"]
+                },
+                "KNeighborsRegressor": {
+                    "n_neighbors": [3,5,7,9]
+                },
 
-            models_data:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            models_data:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
 
             best_model_score=max(sorted(models_data.values()))
             best_model_name=list(models_data.keys())[list(models_data.values()).index(best_model_score)]
@@ -63,10 +113,14 @@ class ModelTrainer:
             
             best_model.fit(x_train,y_train)
             y_pred=best_model.predict(x_test)
+            print(best_model)
             return r2_score(y_test,y_pred)*100
+
 
         except Exception as e:
             raise CustomException(e,sys)
+
+            
 
 
 
--- src/pipelines/utils.py
+++ src/pipelines/utils.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection._search import RandomizedSearchCV
 import os
 import dill
 import sys
@@ -6,6 +7,7 @@ import pandas as pd
 from src.pipelines.exception import CustomException
 from src.pipelines.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,20 +21,30 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         model_list={}
 
-        for i in range(len(list(models.values()))):
-            model=list(models.values())[i]
+        for model_name, model in models.items():
+            if model_name == "CatBoostRegressor":
+                model.fit(x_train, y_train)
+                y_pred = model.predict(x_test)
+                model_list[model_name] = r2_score(y_test, y_pred)
+                continue
+            params=param[model_name]
+            gs=GridSearchCV(estimator=model,param_grid=params,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
             r2_score_train=r2_score(y_train,y_train_pred)
             r2_score_test=r2_score(y_test,y_test_pred)
-            model_list.update({list(models.keys())[i]:r2_score_test})
+            model_list.update({model_name:r2_score_test})
 
         return model_list
 
     except Exception as e:
         raise CustomException(e,sys)
+ 
